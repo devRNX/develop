210750571e04f3dd0928809a097993a2cd535dd8
bishald0013 <bishal986473@gmail.com>
2024-07-17 11:26:30 +0530

    footer changed

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -164,18 +164,6 @@ function Dashboard() {
               </div>
             </div>
           </div>
-          <div className="card-footer shadow-sm text-body-success">
-            <div className="text-center">
-              <p className="mb-0">
-                &copy; 2024 relynrelax. All rights reserved.
-              </p>
-
-              <p className="mb-0">
-                Contact us: helpdesk.relynrelax@gmail.com | Phone: +91
-                9395783957
-              </p>
-            </div>
-          </div>
         </div>
       </div>
     </>
